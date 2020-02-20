6d3de4627662d27decf41fe185d07ad9910fe1ef
Gaurav Lodha <gauravlodha77@gmail.com>
2020-02-21 00:46:39 +0530

    added 3 modules login, bookshelf and reader

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then(m => m.LoginModule)
+  },
+  {
+    path: 'bookshelf',
+    loadChildren: () =>
+      import('./bookshelf/bookshelf.module').then(m => m.BookshelfModule)
+  },
+  {
+    path: 'reader',
+    loadChildren: () =>
+      import('./reader/reader.module').then(m => m.ReaderModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ src/app/reader/reader-routing.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { ReaderComponent } from './reader.component';
+
+const routes: Routes = [{ path: '', component: ReaderComponent }];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class ReaderRoutingModule { }
